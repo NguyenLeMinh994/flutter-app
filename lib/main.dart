26a6d6f96839b9328b2846a9f79592ae50f13d1e
Minh <kurosama99@gmail.com>
2020-08-15 17:54:57 +0700

    doing layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,23 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "XXXXA",
-    home: MyScaffold(),
-  ));
+  runApp(MyApp());
 }
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-  final Widget title;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation menu",
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-              icon: Icon(Icons.search), tooltip: "search", onPressed: null)
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title:
-                Text('XAAA', style: Theme.of(context).primaryTextTheme.title),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello world'),
-            ),
-          )
-        ],
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: "Welcome",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
       ),
     );
   }
