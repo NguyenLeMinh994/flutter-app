63dfe71a8c04dedf3a070366c8cf7b6cce60fb73
Minh <kurosama99@gmail.com>
2020-08-16 11:37:05 +0700

    doing listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Welcome",
       home: Scaffold(
@@ -16,9 +17,58 @@ class MyApp extends StatelessWidget {
           title: Text("Hello"),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWord(),
         ),
       ),
+    );
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  RandomWordState createState() => new RandomWordState();
+}
+
+class RandomWordState extends State<RandomWord> {
+  final List<WordPair> _words = <WordPair>[];
+  final _biggestFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>();
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index.isOdd) {
+          return Divider();
+        }
+
+        if (index >= _words.length) {
+          _words.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_words[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    final bool alreadySaved = _saved.contains(wordPair);
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggestFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
     );
   }
 }
